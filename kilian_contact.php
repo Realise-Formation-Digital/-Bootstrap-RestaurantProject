e2e4f9b55a2ec3c7d4ead56a0985fb8723e25873
Kilian <kilian.gargate@gmail.com>
2020-12-21 18:59:01 +0100

    kilian test contact

---
+++ kilian_contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action='' method='post'>
+<p><label>Name</label><br><input type='text' name='name' value=''></p> 
+<p><label>Email</label><br><input type='text' name='email' value=''></p> 
+<p><input type='submit' name='submit' value='Submit'></p> 
+</form>
+
+
+
+
+
+
+
+    
+</body>
+</html>
